def4d93f83cd2f041acc7f3f0a31d3ab31b10302
STARONE <144688272+MHMOUD-ELBAZ@users.noreply.github.com>
2024-04-25 01:11:01 +0200

    Update HelloApplication.java

--- HelloApplication.java
+++ HelloApplication.java
@@ -1,58 +1,63 @@
 package org.example.gamedemo;
-//abdallah is here
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    private static final double CELL_SIZE = 40;
+    private static final int CELL_SIZE = 40;
     private static final int BOARD_WIDTH = 760;
     private static final int BOARD_HEIGHT = 760;
-
+    private static int ROWS = BOARD_HEIGHT / CELL_SIZE;
+    private static int COLS = BOARD_HEIGHT / CELL_SIZE;
 
     @Override
     public void start(Stage primaryStage) {
-        PacMan pacman = new PacMan(500, 400);
+        // Create Pac-Man
+
+        MazeView mazeView = new MazeView(new Maze(ROWS,COLS));
+        PacMan pacman = new PacMan(1, 5,mazeView);
         Pane root = new Pane();
         Scene scene = new Scene(root, BOARD_WIDTH, BOARD_HEIGHT);
 
-        // Create Pac-Man
-        root.getChildren().add(pacman.getView());
+        root.getChildren().addAll(mazeView.getMazeGroup(),pacman.getView());
 
         // Set up event handling for key presses
-        scene.setOnKeyPressed(event -> {
-            switch (event.getCode()) {
-                case UP:
-                    pacman.movePacman(0, -CELL_SIZE/3);
+        scene.setOnKeyPressed(e->{
+            switch(e.getCode()){
+                case UP :
+                    pacman.moveUp();
                     pacman.rotateToTop();
                     break;
                 case DOWN:
-                    pacman.movePacman(0, CELL_SIZE/3);
+                    pacman.moveDown();
                     pacman.rotateToBottom();
                     break;
-                case LEFT:
-                    pacman.movePacman(-CELL_SIZE/3, 0);
-                    pacman.reflectVerticallyToLeft();
-                    break;
                 case RIGHT:
-                    pacman.movePacman(CELL_SIZE/3, 0);
+                    pacman.moveRight();
                     pacman.reflectVerticallyToRight();
                     break;
-                default:
+                case LEFT:
+                    pacman.moveLeft();
+                    pacman.reflectVerticallyToLeft();
                     break;
             }
         });
-
         primaryStage.setScene(scene);
         primaryStage.setTitle("Pac-Man Game");
         primaryStage.show();
     }
 
+    public static void main(String[] args) {
+        launch();
+    }
 }
+
