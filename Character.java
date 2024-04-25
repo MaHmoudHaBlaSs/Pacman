da6dd9163ef9129b3eecae2e42cf8ede567b44c4
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2024-04-25 20:44:15 +0200

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,66 @@
+package org.example.gamedemo;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Character {
+    protected static final int CELL_SIZE = 40;
+    protected static final int BOARD_WIDTH =760;
+    protected static final int BOARD_HEIGHT = 760;
+    protected Image image;
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    protected ImageView character_gif;
+    protected boolean reflected = false;
+    protected int i;
+    protected int j;
+    protected Maze maze ;
+    protected MazeView mazeView;
+    public Character( MazeView mazeView, Image img){
+        this.mazeView = mazeView;
+        this.image = img;
+        maze = mazeView.getMaze();
+
+
+        // Create the ImageView , stick it to the maze
+        character_gif = new ImageView(img);
+        mazeView.getChildren().add(character_gif);
+
+    }
+    public void reflectVerticallyToLeft() {
+        // Reset to initial angel
+        character_gif.setRotate(0);
+        // Flip the pacman to the left
+        character_gif.setScaleX(-1);
+        reflected = true;
+    }
+    public void rotateToBottom() {
+        if(reflected){
+            character_gif.setScaleX(1);
+        }
+        // Flip the Pacman image horizontally to the bottom
+        character_gif.setRotate(90);
+    }
+    public void reflectVerticallyToRight() {
+        // Flip the Pacman image vertically to the right
+        if(reflected){
+            character_gif.setScaleX(1);
+        }
+        // Reset to initial angel
+        character_gif.setRotate(0);
+    }
+    public void rotateToTop() {
+        if(reflected){
+            character_gif.setScaleX(1);
+        }
+        // Flip the Pacman image horizontally to the top
+        character_gif.setRotate(270);
+    }
+    protected void setPosition(){
+        character_gif.setX(j*CELL_SIZE -5);
+        character_gif.setY(i*CELL_SIZE);
+    }
+}
---
+++ Ghost.java
@@ -0,0 +1,165 @@
+package org.example.gamedemo;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Ghost extends Character{
+    private final int UP =0;
+    private final int DOWN =1;
+    private final int LEFT =2;
+    private final int RIGHT =3;
+    private int direction;
+    private int mode = 1;
+    private int moves =0;
+
+    public void setMode(int mode) {
+        this.mode = mode;
+    }
+    private boolean stuck = false;
+    public Ghost(int startingI,int startingJ,MazeView mazeView){
+        super(mazeView, new Image("Ghost1.gif"));
+        // MazeView extends Pane class.
+        super.mazeView = mazeView;
+        maze = mazeView.getMaze();
+        i = startingI;
+        j = startingJ;
+
+        // Set the size (adjust as needed)
+        character_gif.setFitWidth(CELL_SIZE-8);
+        character_gif.setFitHeight(CELL_SIZE-8);
+
+        // Set the initial position
+        character_gif.setX(j*CELL_SIZE);
+        character_gif.setY(i*CELL_SIZE);
+    }
+    public void moveGhost(){
+        switch(mode)
+        {
+            case 1:
+                // TOP - RIGHT - BOTTOM - LEFT
+                if(moves == 15) {mode = 2; moves = 0;}
+                if(!maze.isWall(i-1,j) && ((direction != DOWN) || isStuck(i,j))) // Check top side
+                {
+                    i -= 1;
+                    setPosition();
+                    direction = UP;
+                }
+                else if(!maze.isWall(i,j+1) && ((direction != LEFT) || (isStuck(i, j)))) // Check right side
+                {
+                    j += 1;
+                    setPosition();
+                    direction = RIGHT;
+                }
+                else if(!maze.isWall(i+1,j) && ((direction != UP) || isStuck(i,j))) // Check bottom side
+                {
+                    i += 1;
+                    setPosition();
+                    direction = DOWN;
+                }
+                else if(!maze.isWall(i, j-1) && ((direction != RIGHT) || (isStuck(i, j)))) // Check left side
+                {
+                    j -= 1;
+                    setPosition();
+                    direction = LEFT;
+                }
+                moves++;
+            break;
+            case 2:
+                // BOTTOM - LEFT -TOP - RIGHT
+                if(moves == 13) {mode = 3; moves = 0;}
+                if(!maze.isWall(i+1,j) && ((direction != UP) || isStuck(i,j))) // Check Bottom side
+                {
+                    i += 1;
+                    setPosition();
+                    direction = DOWN;
+                }
+                else if(!maze.isWall(i, j-1) && ((direction != RIGHT) || (isStuck(i, j)))) // Check lef side
+                {
+                    j -= 1;
+                    setPosition();
+                    direction = LEFT;
+                }
+                else if(!maze.isWall(i-1,j) && ((direction != DOWN) || isStuck(i,j))) // Check top side
+                {
+                    i -= 1;
+                    setPosition();
+                    direction = UP;
+                }
+                else if(!maze.isWall(i,j+1) && ((direction != LEFT) || (isStuck(i, j)))) // Check right side
+                {
+                    j += 1;
+                    setPosition();
+                    direction = RIGHT;
+                }
+                moves++;
+            break;
+            case 3:
+                // LEFT - TOP - BOTTOM - RIGHT
+                if(moves == 12) {mode = 4; moves = 0;}
+                if(!maze.isWall(i, j-1) && ((direction != RIGHT) || (isStuck(i, j)))) // Check lef side
+                {
+                    j -= 1;
+                    setPosition();
+                    direction = LEFT;
+                }
+                else if(!maze.isWall(i-1,j) && ((direction != DOWN) || isStuck(i,j))) // Check top side
+                {
+                    i -= 1;
+                    setPosition();
+                    direction = UP;
+                }
+                else if(!maze.isWall(i+1,j) && ((direction != UP) || isStuck(i,j))) // Check Bottom side
+                {
+                    i += 1;
+                    setPosition();
+                    direction = DOWN;
+                }
+                else if(!maze.isWall(i,j+1) && ((direction != LEFT) || (isStuck(i, j)))) // Check right side
+                {
+                    j += 1;
+                    setPosition();
+                    direction = RIGHT;
+                }
+                moves++;
+            break;
+            case 4:
+                // RIGHT - BOTTOM - TOP - LEFT
+                if(moves == 14) {mode = 1; moves = 0;}
+                if(!maze.isWall(i,j+1) && ((direction != LEFT) || (isStuck(i, j)))) // Check right side
+                {
+                    j += 1;
+                    setPosition();
+                    direction = RIGHT;
+                }
+                else if(!maze.isWall(i+1,j) && ((direction != UP) || isStuck(i,j))) // Check Bottom side
+                {
+                    i += 1;
+                    setPosition();
+                    direction = DOWN;
+                }
+                else if(!maze.isWall(i-1,j) && ((direction != DOWN) || isStuck(i,j))) // Check top side
+                {
+                    i -= 1;
+                    setPosition();
+                    direction = UP;
+                }
+                else if(!maze.isWall(i, j-1) && ((direction != RIGHT) || (isStuck(i, j)))) // Check lef side
+                {
+                    j -= 1;
+                    setPosition();
+                    direction = LEFT;
+                }
+                moves++;
+            break;
+        }
+    }
+
+    public boolean isStuck(int i, int j){
+        int walls = 0;
+        if(maze.isWall(i-1,j)) {walls++;} // Check top side
+        if(maze.isWall(i,j+1)) {walls++;} // Check right side
+        if(maze.isWall(i+1,j)) {walls++;} // Check bottom side
+        if(maze.isWall(i, j-1)) {walls++;}// Check left side
+        return walls == 3;
+    }
+}
