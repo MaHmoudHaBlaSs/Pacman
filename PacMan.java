75ad5d0f380d5df5a7d15d9780221f192e2fd5e1
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2024-04-25 20:42:59 +0200

    Update PacMan.java

--- PacMan.java
+++ PacMan.java
@@ -1,42 +1,28 @@
-package com.example.pac_man;
+package org.example.gamedemo;
 
-import javafx.scene.Node;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 
-public class PacMan extends Pane {
-    private static final int CELL_SIZE = 40;
-    private static final int BOARD_WIDTH =760;
-    private static final int BOARD_HEIGHT = 760;
-    private Image image;
-    private ImageView pacman_gif;
-    private boolean reflected = false;
-    private int i;
-    private int j;
-    private Maze maze ;
-    private MazeView mazeView;
+public class PacMan extends Character{
     private int score = 0;
+
     public PacMan(int startingI,int startingJ,MazeView mazeView){
-        this.mazeView = mazeView;
+        super(mazeView, new Image("pacman.gif"));
+        // MazeView extends Pane class.
+        super.mazeView = mazeView;
         maze = mazeView.getMaze();
         i = startingI;
         j = startingJ;
 
-        // Create the ImageView , stick it to the maze
-        pacman_gif = new ImageView("pacman02.gif");
-        mazeView.getChildren().add(pacman_gif);
+        // Set the size (adjust as needed)
+        character_gif.setFitWidth(CELL_SIZE+10);
+        character_gif.setFitHeight(CELL_SIZE+10);
 
         // Set the initial position
-        pacman_gif.setX(j*CELL_SIZE-5);
-        pacman_gif.setY(i*CELL_SIZE);
-
-        // Set the size (adjust as needed)
-        pacman_gif.setFitWidth(CELL_SIZE+10);
-        pacman_gif.setFitHeight(CELL_SIZE+10);
+        character_gif.setX(j*CELL_SIZE-5);
+        character_gif.setY(i*CELL_SIZE);
 
     }
 
@@ -69,47 +55,11 @@ public class PacMan extends Pane {
         }
     }
 
-    public void reflectVerticallyToLeft() {
-        // Reset to initial angel
-        pacman_gif.setRotate(0);
-        // Flip the pacman to the left
-        pacman_gif.setScaleX(-1);
-        reflected = true;
-    }
-
-    public void rotateToBottom() {
-        if(reflected){
-            pacman_gif.setScaleX(1);
-        }
-        // Flip the Pacman image horizontally to the bottom
-        pacman_gif.setRotate(90);
-    }
-    public void reflectVerticallyToRight() {
-        // Flip the Pacman image vertically to the right
-        if(reflected){
-            pacman_gif.setScaleX(1);
-        }
-        // Reset to initial angel
-        pacman_gif.setRotate(0);
-    }
-
-    public void rotateToTop() {
-        if(reflected){
-            pacman_gif.setScaleX(1);
-        }
-        // Flip the Pacman image horizontally to the top
-        pacman_gif.setRotate(270);
-    }
-
-    private void setPosition(){
-        pacman_gif.setX(j*CELL_SIZE -5);
-        pacman_gif.setY(i*CELL_SIZE);
-    }
     private void updateScore(){
         if(maze.isPellet(i,j))
-            score += 1;
+            score += 50;
         else if(maze.isPowerPellet(i,j))
-            score += 3;
+            score += 100;
 
         //replace the pallet cell with empty space
         Rectangle emptySpace = new Rectangle(j * CELL_SIZE, i * CELL_SIZE,CELL_SIZE,CELL_SIZE);
