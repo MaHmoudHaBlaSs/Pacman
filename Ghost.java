ca7f52b54c3bd0eeb72c6566791456e7aa1e7f7e
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2024-05-01 17:49:08 +0300

    Update Ghost.java

--- Ghost.java
+++ Ghost.java
@@ -1,127 +1,179 @@
 package org.example.gamedemo;
 
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
-public class Ghost extends Character{
-    private final int UP =0;
-    private final int DOWN =1;
-    private final int LEFT =2;
-    private final int RIGHT =3;
-    private int direction;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+import java.util.List;
+
+public class Ghost extends Character {
+    MazeView mazeView;
+    Maze maze;
+    PacMan pacMan ;
+    Timeline ghostMovement;
+    private enum Direction {
+        UP ,DOWN,RIGHT,LEFT;
+    }
+    private Direction direction;
     private int mode = 1;
     private int moves =0;
-
-    public void setMode(int mode) {
-        this.mode = mode;
-    }
-    private boolean stuck = false;
-    public Ghost(int startingI,int startingJ,MazeView mazeView){
-        super(mazeView, new Image("Ghost1.gif"));
-        // MazeView extends Pane class.
-        super.mazeView = mazeView;
+    private int counter =0;
+    private int[][] steps;
+    public Ghost(int startingI, int startingJ, MazeView mazeView,int level) {
+        this.mazeView = mazeView;
         maze = mazeView.getMaze();
-        i = startingI;
-        j = startingJ;
+        currentRow = startingI;
+        currentColumn = startingJ;
+
+        // get the pacman
+        for(Node node:mazeView.getChildren()){
+            if(node instanceof PacMan) {
+                pacMan = (PacMan) node;
+            }
+        }
+
+        // Create the ImageView , stick it to the maze
+        ImageView gif = switch (level) {
+            case 1,2,3 -> new ImageView("Ghost1.gif"); // Normal Ghost
+            case 4,5 -> new ImageView("Ghost2.gif"); // Directed Ghost
+            default -> null;
+        };
+        mazeView.getChildren().add(gif);
+        setGif(gif);
+
+        // Set the initial position for the ghost
+        setPosition();
 
         // Set the size (adjust as needed)
-        character_gif.setFitWidth(CELL_SIZE-8);
-        character_gif.setFitHeight(CELL_SIZE-8);
+        gif.setFitWidth(CELL_SIZE-5);
+        gif.setFitHeight(CELL_SIZE-5);
 
-        // Set the initial position
-        character_gif.setX(j*CELL_SIZE);
-        character_gif.setY(i*CELL_SIZE);
+        //animate the ghost
+
+        switch(level){
+            case 1:
+                ghostMovement = new Timeline(new KeyFrame(Duration.millis(500),e->moveGhostRandomly()) );
+                ghostMovement.setCycleCount(-1);
+                ghostMovement.play();
+                break;
+            case 2:
+                ghostMovement = new Timeline(new KeyFrame(Duration.millis(400),e->moveGhostRandomly()) );
+                ghostMovement.setCycleCount(-1);
+                ghostMovement.play();
+                break;
+            case 3:
+                ghostMovement = new Timeline(new KeyFrame(Duration.millis(300),e-> moveGhostRandomly()));
+                ghostMovement.setCycleCount(-1);
+                ghostMovement.play();
+                break;
+            case 4:
+                ghostMovement = new Timeline(new KeyFrame(Duration.millis(400),e-> moveDirectedGhost()) );
+                ghostMovement.setCycleCount(-1);
+                ghostMovement.play();
+                break;
+            case 5:
+                ghostMovement = new Timeline(new KeyFrame(Duration.millis(300),e->moveDirectedGhost()));
+                ghostMovement.setCycleCount(-1);
+                ghostMovement.play();
+                break;
+        }
     }
-    public void moveGhost(){
+    public void setMode(int mode){
+        this.mode = mode;
+    }
+    private void moveGhostRandomly(){
         switch(mode)
         {
             case 1:
                 // TOP - RIGHT - BOTTOM - LEFT
                 if(moves == 15) {mode = 2; moves = 0;}
-                if(!maze.isWall(i-1,j) && ((direction != DOWN) || isStuck(i,j))) // Check top side
+                if(!maze.isWall(currentRow-1,currentColumn) && ((direction != Direction.DOWN) || isStuck(currentRow,currentColumn))) // Check top side
                 {
                     moveUp();
                 }
-                else if(!maze.isWall(i,j+1) && ((direction != LEFT) || (isStuck(i, j)))) // Check right side
+                else if(!maze.isWall(currentRow,currentColumn+1) && ((direction != Direction.LEFT) || (isStuck(currentRow, currentColumn)))) // Check right side
                 {
                     moveRight();
                 }
-                else if(!maze.isWall(i+1,j) && ((direction != UP) || isStuck(i,j))) // Check bottom side
+                else if(!maze.isWall(currentRow+1,currentColumn) && ((direction != Direction.UP) || isStuck(currentRow,currentColumn))) // Check bottom side
                 {
                     moveDown();
                 }
-                else if(!maze.isWall(i, j-1) && ((direction != RIGHT) || (isStuck(i, j)))) // Check left side
+                else if(!maze.isWall(currentRow, currentColumn-1) && ((direction != Direction.RIGHT) || (isStuck(currentRow, currentColumn)))) // Check left side
                 {
                     moveLeft();
                 }
                 moves++;
-            break;
+                break;
             case 2:
                 // BOTTOM - LEFT -TOP - RIGHT
                 if(moves == 13) {mode = 3; moves = 0;}
-                if(!maze.isWall(i+1,j) && ((direction != UP) || isStuck(i,j))) // Check Bottom side
+                if(!maze.isWall(currentRow+1,currentColumn) && ((direction != Direction.UP) || isStuck(currentRow,currentColumn))) // Check Bottom side
                 {
                     moveDown();
                 }
-                else if(!maze.isWall(i, j-1) && ((direction != RIGHT) || (isStuck(i, j)))) // Check lef side
+                else if(!maze.isWall(currentRow, currentColumn-1) && ((direction != Direction.RIGHT) || (isStuck(currentRow, currentColumn)))) // Check lef side
                 {
                     moveLeft();
                 }
-                else if(!maze.isWall(i-1,j) && ((direction != DOWN) || isStuck(i,j))) // Check top side
+                else if(!maze.isWall(currentRow-1,currentColumn) && ((direction != Direction.DOWN) || isStuck(currentRow,currentColumn))) // Check top side
                 {
                     moveUp();
                 }
-                else if(!maze.isWall(i,j+1) && ((direction != LEFT) || (isStuck(i, j)))) // Check right side
+                else if(!maze.isWall(currentRow,currentColumn+1) && ((direction != Direction.LEFT) || (isStuck(currentRow, currentColumn)))) // Check right side
                 {
                     moveRight();
                 }
                 moves++;
-            break;
+                break;
             case 3:
                 // LEFT - TOP - BOTTOM - RIGHT
                 if(moves == 12) {mode = 4; moves = 0;}
-                if(!maze.isWall(i, j-1) && ((direction != RIGHT) || (isStuck(i, j)))) // Check lef side
+                if(!maze.isWall(currentRow, currentColumn-1) && ((direction != Direction.RIGHT) || (isStuck(currentRow, currentColumn)))) // Check lef side
                 {
                     moveLeft();
                 }
-                else if(!maze.isWall(i-1,j) && ((direction != DOWN) || isStuck(i,j))) // Check top side
+                else if(!maze.isWall(currentRow-1,currentColumn) && ((direction != Direction.DOWN) || isStuck(currentRow,currentColumn))) // Check top side
                 {
                     moveUp();
                 }
-                else if(!maze.isWall(i+1,j) && ((direction != UP) || isStuck(i,j))) // Check Bottom side
+                else if(!maze.isWall(currentRow+1,currentColumn) && ((direction != Direction.UP) || isStuck(currentRow,currentColumn))) // Check Bottom side
                 {
                     moveDown();
                 }
-                else if(!maze.isWall(i,j+1) && ((direction != LEFT) || (isStuck(i, j)))) // Check right side
+                else if(!maze.isWall(currentRow,currentColumn+1) && ((direction != Direction.LEFT) || (isStuck(currentRow, currentColumn)))) // Check right side
                 {
                     moveRight();
                 }
                 moves++;
-            break;
+                break;
             case 4:
                 // RIGHT - BOTTOM - TOP - LEFT
                 if(moves == 14) {mode = 1; moves = 0;}
-                if(!maze.isWall(i,j+1) && ((direction != LEFT) || (isStuck(i, j)))) // Check right side
+                if(!maze.isWall(currentRow,currentColumn+1) && ((direction != Direction.LEFT) || (isStuck(currentRow, currentColumn)))) // Check right side
                 {
                     moveRight();
                 }
-                else if(!maze.isWall(i+1,j) && ((direction != UP) || isStuck(i,j))) // Check Bottom side
+                else if(!maze.isWall(currentRow+1,currentColumn) && ((direction != Direction.UP) || isStuck(currentRow,currentColumn))) // Check Bottom side
                 {
                     moveDown();
                 }
-                else if(!maze.isWall(i-1,j) && ((direction != DOWN) || isStuck(i,j))) // Check top side
+                else if(!maze.isWall(currentRow-1,currentColumn) && ((direction != Direction.DOWN) || isStuck(currentRow,currentColumn))) // Check top side
                 {
                     moveUp();
                 }
-                else if(!maze.isWall(i, j-1) && ((direction != RIGHT) || (isStuck(i, j)))) // Check lef side
+                else if(!maze.isWall(currentRow, currentColumn-1) && ((direction != Direction.RIGHT) || (isStuck(currentRow, currentColumn)))) // Check lef side
                 {
                     moveLeft();
                 }
                 moves++;
-            break;
+                break;
         }
     }
-
     public boolean isStuck(int i, int j){
         int walls = 0;
         if(maze.isWall(i-1,j)) {walls++;} // Check top side
@@ -130,28 +182,53 @@ public class Ghost extends Character{
         if(maze.isWall(i, j-1)) {walls++;}// Check left side
         return walls == 3;
     }
-    @Override
     public void moveRight(){
-        j += 1;
+        currentColumn += 1;
         setPosition();
-        direction = RIGHT;
+        direction = Direction.RIGHT;
     }
-    @Override
     public void moveLeft(){
-        j -= 1;
+        currentColumn -= 1;
         setPosition();
-        direction = LEFT;
+        direction = Direction.LEFT;
     }
-    @Override
     public void moveUp(){
-        i -= 1;
+        currentRow -= 1;
         setPosition();
-        direction = UP;
+        direction = Direction.UP;
     }
-    @Override
     public void moveDown(){
-        i += 1;
+        currentRow += 1;
         setPosition();
-        direction = DOWN;
+        direction = Direction.DOWN;
+    }
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+    private void setPath(){
+        //start: ghost location , end: pacMan location
+        int start = maze.indicesToCell(this.currentRow,this.currentColumn);
+        int end = maze.indicesToCell(pacMan.getCurrentRow(), pacMan.getCurrentColumn());
+
+        //find the shortest path, set the steps
+        List<Integer> list = maze.findShortestPath(start,end);
+        int length = list.size();
+        steps = new int[length][2];
+        for(int i=0;i<length;i++){
+            steps[i] = maze.cellToIndices(list.get(i));
+        }
+
+        //return counter to 0
+        counter=0;
+    }
+    private void moveDirectedGhost(){
+        if(counter%9 ==0 )
+            setPath();
+
+        if (counter<steps.length){
+            currentRow = steps[counter][0];
+            currentColumn = steps[counter][1];
+            setPosition();
+        }
+        counter++;
     }
 }
