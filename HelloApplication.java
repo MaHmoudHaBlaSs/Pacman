383b8916c45ad3d5d389fd2330cc5805c19e029e
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2024-05-01 17:50:04 +0300

    Update HelloApplication.java

--- HelloApplication.java
+++ HelloApplication.java
@@ -4,36 +4,43 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-
-import java.io.File;
 
 public class HelloApplication extends Application {
     private static final int CELL_SIZE = 40;
     private static final int BOARD_WIDTH = 760;
     private static final int BOARD_HEIGHT = 760;
+    int level = 5;
+
     @Override
     public void start(Stage primaryStage) {
-
+        // Create maze , Pac-Man, ghosts
         MazeView mazeView = new MazeView(new Maze(BOARD_WIDTH,BOARD_HEIGHT,CELL_SIZE));
-        PacMan pacman = new PacMan(1, 1,mazeView); // Our Hero
-        Ghost ghost1 = new Ghost(7,8,mazeView);
-        ghost1.setMode(1);
-        Ghost ghost2 = new Ghost(17,1,mazeView);
-        ghost2.setMode(2);
-        Ghost ghost3 = new Ghost(17,17,mazeView);
-        ghost3.setMode(3);
-        Ghost ghost4 = new Ghost(8,9,mazeView);
-        ghost4.setMode(4);
+        PacMan pacman = new PacMan(1, 1,mazeView);
+        Ghost[] ghosts = createGhosts(mazeView,level);
+        ImageView backgroundImg = new ImageView(new Image("Background.gif"));
+        backgroundImg.setFitWidth(BOARD_WIDTH);
+        backgroundImg.setFitHeight(BOARD_HEIGHT);
+        StackPane backgroundPane = new StackPane(backgroundImg);
+        backgroundPane.getChildren().add(mazeView);
+        //check if the pacman is alive
+        Timeline positionChecker = new Timeline(new KeyFrame(Duration.millis(50), e->{
+            for(Ghost ghost : ghosts)
+                if(ghost.getCurrentColumn() == pacman.getCurrentColumn() && ghost.getCurrentRow() == pacman.getCurrentRow())
+                    primaryStage.close();
+        }));
+        positionChecker.setCycleCount(-1);
+        positionChecker.play();
 
-        //adding backgroud sound
 
+        //game scene
+        Scene scene = new Scene(backgroundPane, BOARD_WIDTH, BOARD_HEIGHT);
 
-
-        Scene scene = new Scene(mazeView, BOARD_WIDTH, BOARD_HEIGHT);
 
         // Set up event handling for key presses
         scene.setOnKeyPressed(e->{
@@ -46,7 +53,6 @@ public class HelloApplication extends Application {
                     pacman.moveDown();
                     pacman.rotateToBottom();
                     break;
-
                 case RIGHT:
                     pacman.moveRight();
                     pacman.reflectVerticallyToRight();
@@ -57,40 +63,62 @@ public class HelloApplication extends Application {
                     break;
             }
         });
-
-        Timeline ghostTM = new Timeline(
-                new KeyFrame(Duration.millis(400), event -> {
-                    Media sound = new Media(new File("C:\\Users\\Abdal\\IdeaProjects\\helloapplication\\src\\main\\resources\\pacman_chomp.mp3").toURI().toString());
-                    MediaPlayer bckgmp3 = new MediaPlayer(sound);
-                    bckgmp3.setCycleCount(bckgmp3.INDEFINITE);
-                    bckgmp3.play();
-                    ghost1.moveGhost();
-                    ghost2.moveGhost();
-                    ghost3.moveGhost();
-                    ghost4.moveGhost();
-                }));
-        ghostTM.setCycleCount(-1);
-        ghostTM.play();
-
-        Timeline checklife = new Timeline(
-                new KeyFrame(Duration.millis(2 ), e -> {
-
-                    if((pacman.getI()==ghost1.getI()&&pacman.getJ()==ghost1.getJ())
-                       ||(pacman.getI()==ghost2.getI()&&pacman.getJ()==ghost2.getJ())
-                       ||(pacman.getI()==ghost3.getI()&&pacman.getJ()==ghost3.getJ())
-                       ||(pacman.getI()==ghost4.getI()&&pacman.getJ()==ghost4.getJ())){
-                        mazeView.getChildren().remove(pacman.character_gif);
-                        ghostTM.stop();
-
-                    }
-                }));
-        checklife.setCycleCount(-1);
-        checklife.play();
-
         primaryStage.setScene(scene);
         primaryStage.setTitle("Pac-Man Game");
         primaryStage.show();
+
+//        Timeline checklife = new Timeline(
+//                new KeyFrame(Duration.millis(2 ), e -> {
+//                    if((pacman.getCurrentRow()==ghost1.getI()&&pacman.getJ()==ghost1.getJ())
+//                            ||(pacman.getCurrentRow()==ghost2.getI()&&pacman.getCurrentColumn()==ghost2.getJ())
+//                            ||(pacman.getCurrentRow()==ghost3.getI()&&pacman.getCurrentColumn()==ghost3.getJ())
+//                            ||(pacman.getCurrentRow()==ghost4.getI()&&pacman.getCurrentColumn()==ghost4.getJ())){
+//                        mazeView.getChildren().remove(pacman.character_gif);
+//                        ghostTM.stop();
+//
+//                    }
+//                }));
+//        checklife.setCycleCount(-1);
+//        checklife.play();
+    }
+    public Ghost[] createGhosts(MazeView mazeView,int level){
+        Ghost[] ghosts = new Ghost[4];
+        switch (level){
+            case 1,2,3:
+                ghosts[0] = new Ghost(17,17,mazeView, level);
+                ghosts[0].setMode(1);
+                ghosts[1] = new Ghost(17,1,mazeView, level);
+                ghosts[1].setMode(2);
+                ghosts[2] = new Ghost(1,17,mazeView, level);
+                ghosts[2].setMode(3);
+                ghosts[3] = new Ghost(9,6,mazeView, level);
+                break;
+
+            case 4:
+                ghosts[0] = new Ghost(17,17,mazeView, 2);
+                ghosts[0].setMode(1);
+                ghosts[1] = new Ghost(17,1,mazeView, 3);
+                ghosts[1].setMode(2);
+                ghosts[2] = new Ghost(1,17,mazeView, 3);
+                ghosts[2].setMode(3);
+                ghosts[3] = new Ghost(9,6,mazeView, level);
+                break;
+            case 5:
+                ghosts[0] = new Ghost(17,17,mazeView, 3);
+                ghosts[0].setMode(1);
+                ghosts[1] = new Ghost(17,1,mazeView, 3);
+                ghosts[1].setMode(2);
+                ghosts[2] = new Ghost(1,17,mazeView, 3);
+                ghosts[2].setMode(3);
+                ghosts[3] = new Ghost(9,6,mazeView, level);
+                break;
+
+
+        }
+        return ghosts;
     }
 
-
+    public static void main(String[] args) {
+        launch();
+    }
 }
