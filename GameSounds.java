1638af67f3905c85626f8a9de3d5b9b0ae7764d1
Abdallahmoh443 <139957345+Abdallahmoh443@users.noreply.github.com>
2024-05-06 17:03:18 +0300

    Update GameSounds.java
    add new sound

--- GameSounds.java
+++ GameSounds.java
@@ -14,32 +14,43 @@ public class GameSounds extends Pane {
     MediaPlayer eatPellet ;
     MediaPlayer deathSound;
     MediaPlayer btnSound;
+    MediaPlayer winSound;
     public GameSounds() {
         // create background music
-        File soundFile = new File("C:\\Users\\Abdal\\IdeaProjects\\helloapplication\\src\\main\\resources\\start.mp3");
+        File soundFile = new File("C:\\Users\\Abdal\\IdeaProjects\\pacman\\src\\main\\resources\\start.mp3");
         Media sound = new Media(soundFile.toURI().toString());
         start_sound = new MediaPlayer(sound);
         start_sound.setVolume(.3);
 
 
         // create eat PELLET & POWERPELLET sound
-        File eatPelletFile = new File("C:\\Users\\Abdal\\IdeaProjects\\helloapplication\\src\\main\\resources\\munch_1.mp3");
+        File eatPelletFile = new File("C:\\Users\\Abdal\\IdeaProjects\\pacman\\src\\main\\resources\\munch_1.mp3");
         Media soundEatPELLET = new Media(eatPelletFile.toURI().toString());
         eatPellet = new MediaPlayer(soundEatPELLET);
         eatPellet.setVolume(.7);
 
 
         // create death sound
-        File death = new File("C:\\Users\\Abdal\\IdeaProjects\\helloapplication\\src\\main\\resources\\pacman_death.mp3");
+        File death = new File("C:\\Users\\Abdal\\IdeaProjects\\pacman\\src\\main\\resources\\over.mp3");
         Media death_Sound = new Media(death.toURI().toString());
         deathSound = new MediaPlayer(death_Sound);
-        deathSound.setVolume(.5);
+        deathSound.setVolume(1);
+        deathSound.setCycleCount(1);
 
         //create buttons sound
-        File btnFile = new File("C:\\Users\\Abdal\\IdeaProjects\\helloapplication\\src\\main\\resources\\btnSound.mp3");
+        File btnFile = new File("C:\\Users\\Abdal\\IdeaProjects\\pacman\\src\\main\\resources\\btnSound.mp3");
         Media btn = new Media(btnFile.toURI().toString());
         btnSound = new MediaPlayer(btn);
         btnSound.setVolume(.5);
+
+
+        //create win sound
+        File WinFile = new File("C:\\Users\\Abdal\\IdeaProjects\\pacman\\src\\main\\resources\\win.mp3");
+        Media win = new Media(WinFile.toURI().toString());
+        winSound = new MediaPlayer(win);
+        winSound.setVolume(1);
+        winSound.setCycleCount(1);
+
 
 
 
