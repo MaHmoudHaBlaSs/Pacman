fa51b014af612b700f8296a13c3d1783b6ce47ac
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2024-05-09 18:42:42 +0300

    Update MazeView.java

--- MazeView.java
+++ MazeView.java
@@ -1,6 +1,7 @@
 package org.example.gamedemo;
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -9,15 +10,17 @@ import javafx.scene.shape.Circle;
 public class MazeView extends Pane{
     private static final int CELL_SIZE = 40; // Controls the screen Dimensions (be Careful)
     private Maze maze;
+    private String[] gatesUrls = {"Gate1.gif","Gate2.gif"};
     public MazeView(Maze maze) {
         this.maze = maze;
-        drawMaze();
+
+        drawMaze(maze.getMazeNum());
     }
     public Maze getMaze(){
         return maze;
     }
 
-    private void drawMaze() {
+    private void drawMaze(int mazeNum) {
         int rows = maze.getRows();
         int cols = maze.getCols();
 
@@ -27,12 +30,43 @@ public class MazeView extends Pane{
                 {
                     //                Coordinates : X(Rows*Width), Y(Cols*Height)      Dimensions
                     Rectangle wall = new Rectangle(j * CELL_SIZE, i * CELL_SIZE, CELL_SIZE-2, CELL_SIZE-2);
-                    wall.setFill(Color.rgb(36,36,50)); // Adjust wall color
-                    wall.setStroke(Color.BLACK);
+                    switch (mazeNum){
+                        case 1:
+                            wall.setFill(Color.web("#808080").deriveColor(0, 1, 1,1)); // Adjust wall color
+                            wall.setStroke(Color.BLACK);
+                        break;
+                        case 2:
+                            wall.setFill(Color.web("#922724").deriveColor(0, 1, 1, 1)); // Adjust wall color
+                            wall.setStroke(Color.ORANGE);
+                        break;
+                        case 3:
+                            wall.setFill(Color.web("#c0c0c0").deriveColor(0, 1, 1, 1)); // Adjust wall color
+                            wall.setStroke(Color.BLACK);
+                        break;
+                    }
+
                     wall.setStrokeWidth(3);
                     wall.setArcHeight(10);
                     wall.setArcWidth(10);
                     getChildren().add(wall);
+                }
+                else if(maze.isGateIn(i, j)){
+                    ImageView gateIn = new ImageView(gatesUrls[0]);
+                    gateIn.setFitHeight(CELL_SIZE-10);
+                    gateIn.setFitWidth(CELL_SIZE+5);
+                    gateIn.setTranslateX(j*CELL_SIZE);
+                    gateIn.setTranslateY(i*CELL_SIZE);
+                    if(j<9){gateIn.setScaleX(-1);}
+                    getChildren().add(gateIn);
+                }
+                else if(maze.isGateOut(i, j)){
+                    ImageView gateOut = new ImageView(gatesUrls[1]);
+                    gateOut.setFitHeight(CELL_SIZE-10);
+                    gateOut.setFitWidth(CELL_SIZE+5);
+                    gateOut.setTranslateX(j*CELL_SIZE);
+                    gateOut.setTranslateY(i*CELL_SIZE);
+                    if(j<9){gateOut.setScaleX(-1);}
+                    getChildren().add(gateOut);
                 }
                 else if (maze.isPellet(i, j))
                 {
