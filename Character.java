a67fb7f2371d11a12cd715fd6d3018337a7f005f
Abdallahmoh443 <139957345+Abdallahmoh443@users.noreply.github.com>
2024-05-14 18:35:39 +0300

    Update Character.java

--- Character.java
+++ Character.java
@@ -11,43 +11,44 @@ public class Character extends Pane {
     private ImageView gif ;
     protected int currentRow = 0;
     protected int currentColumn = 0;
+    protected int nextRow = 0;
+    protected int nextColumn = 0;
     private boolean reflected = false;
-    private Timeline mover ; // Responsible for Smooth Transition for the Gif
+    private Timeline mover ;
     private double dx;
     private double dy;
     private Timeline setPositionTm;
+    int counter = 0;
+    protected Direction direction;
 
     public Character(){
+        mover =  new Timeline(new KeyFrame(Duration.millis(10), e-> move(dx,dy)));
+        mover.setCycleCount(CELL_SIZE/2);
+        mover.setOnFinished( e->{
+            counter =0;
+        });
 
-        // Changes Character Gif Position Every 10ms by 2 Pixels For The Cycle So It Should be Played For 20 Times to Reach 40 Pixels(CELL_WIDTH)
-        // [Doesn't Change currentRow - currentColumn]
-        mover =  new Timeline(new KeyFrame(Duration.millis(10), e-> {
-            gif.setY(gif.getY() + dy * 2);
-            gif.setX(gif.getX() + dx * 2);
-        }));
-        mover.setCycleCount(CELL_SIZE/2); // Playing For 20 Cycles.
+        setPositionTm = new Timeline(new KeyFrame(Duration.millis(250),e->{
+            gif.setX(currentColumn*CELL_SIZE +5);
+            gif.setY(currentRow*CELL_SIZE);}
+        ));
 
-        // Changes Character Gif Coordinates Through a Certain Amount of Time (190ms)
-        // [Doesn't Change currentRow - currentColumn]
-        setPositionTm = new Timeline(new KeyFrame(Duration.millis(190),e->{
-            gif.setX(currentColumn*CELL_SIZE -5);
-            gif.setY(currentRow*CELL_SIZE);
-        }));
-        setPositionTm.setCycleCount(1); // Playing Once Per Move (By Invoking 'setPosition' Method).
+        setPositionTm.setCycleCount(1);
     }
-    public int getCurrentColumn() {return currentColumn;}
-    public int getCurrentRow() {return currentRow;}
-    public ImageView getGif(){return gif;}
-    protected void setPosition(){setPositionTm.play();}
-    public void setGif(ImageView gif) {this.gif = gif;}
-    protected void setPosition(int row,int col){
+    public int getCurrentColumn() {
+        return currentColumn;
+    }
 
-        // One of dx Or dy Will Equal to 1 And Another Will Be 0
-        dx = col-currentColumn;
-        dy = row -currentRow;
-        // Update the Position
-        currentColumn = col;
-        currentRow = row;
+    public int getCurrentRow() {
+        return currentRow;
+    }
+
+    protected void setPosition(int row,int col){
+        dx = col-currentColumn ;
+        dy = row -currentRow ;
+
+        nextColumn = col;
+        nextRow = row;
 
         mover.play();
     }
@@ -55,8 +56,22 @@ public class Character extends Pane {
     private void move(double dx , double dy){
         gif.setY(gif.getY() + dy * 2);
         gif.setX(gif.getX() + dx * 2);
+        counter++;
+        if(counter == 13){
+            currentRow = nextRow;
+            currentColumn = nextColumn;
+        }
+    }
+    protected void setPosition(){
+        setPositionTm.play();
     }
 
+    public void setGif(ImageView gif) {
+        this.gif = gif;
+    }
+    public ImageView getGif(){
+        return gif;
+    }
     public void reflectVerticallyToLeft() {
         // Reset to initial angel
         gif.setRotate(0);
@@ -86,5 +101,10 @@ public class Character extends Pane {
         // Flip the Pacman image horizontally to the top
         gif.setRotate(270);
     }
-
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+    public Direction getDirection() {
+        return direction;
+    }
 }
