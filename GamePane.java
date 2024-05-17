f1d5afccb2ae1be529647a24a150cebbe5ad4d82
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2024-05-17 21:11:34 +0300

    Update GamePane.java

--- GamePane.java
+++ GamePane.java
@@ -1,4 +1,4 @@
-package com.example.pac_man;
+package org.example.gamedemo;
 
 import javafx.animation.*;
 import javafx.scene.Scene;
@@ -10,45 +10,33 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class GamePane extends Pane {
-    public static final int CELL_SIZE = 40;
-    public static final int BOARD_WIDTH = 760;
-    public static final int BOARD_HEIGHT = 760;
-    private MazeView mazeView;
-    private PacMan pacman ;
-    GameSounds sound ;
+    private static final int CELL_SIZE = 40;
+    private static final int BOARD_WIDTH = 760;
+    private static final int BOARD_HEIGHT = 760;
+    private final PacMan pacman ;
+    private final Stage stage ;
+    private final Scene mainScene;
+    private final Scene gameScene;
+    private final GameSounds sound ;
     private final Ghost[] ghosts;
-    boolean death = false;   // to know pacman is still alive or die
-    private boolean win = false;      // to know if user win
-    private Stage stage ;
-    private Scene mainScene;
-    private Scene gameScene;
     private Timeline positionChecker;
+    private boolean death = false; // To Know Pacman is still alive or die
 
-    public GamePane( int mapNumber, GameSounds sound , int level, int pacmanGifNum,Stage stage,Scene mainScene ) {
+    public GamePane(int mapNumber, GameSounds sound , int level, int pacmanGifNum, Stage stage, Scene mainScene ) {
         this.sound = sound;
         this.stage = stage;
         this.mainScene = mainScene;
 
-        mazeView = new MazeView(new Maze(BOARD_WIDTH, BOARD_HEIGHT, CELL_SIZE, mapNumber));
+        MazeView mazeView = new MazeView(new Maze(BOARD_WIDTH, BOARD_HEIGHT, CELL_SIZE, mapNumber));
         pacman = new PacMan(1, 1, mazeView, pacmanGifNum);
         ghosts = createGhosts(mazeView, level);
-        getChildren().addAll(createBackground(BOARD_WIDTH,BOARD_HEIGHT,mapNumber),mazeView);
+        getChildren().addAll(createBackground(mapNumber),mazeView);
         gameScene = new Scene(this,BOARD_WIDTH,BOARD_HEIGHT);
 
         movePacman();
         checkLife();
-
     }
 
-
-    // Creating Game Background
-    private ImageView createBackground(double width, double height, int mazeNum) {
-        String[] mazes = new String[]{"GameBackground1.gif", "GameBackground2.gif", "GameBackground3.gif"};
-        ImageView background = new ImageView(mazes[mazeNum - 1]);
-        background.setFitWidth(width);
-        background.setFitHeight(height);
-        return background;
-    }
     // Returning ghosts (Array) created based on level
     public Ghost[] createGhosts(MazeView mazeView, int level) {
         Ghost[] ghosts = new Ghost[4];
@@ -60,7 +48,7 @@ public class GamePane extends Pane {
                 ghosts[1].setMode(2);
                 ghosts[2] = new Ghost(1, 17, mazeView, level);
                 ghosts[2].setMode(3);
-                ghosts[3] = new Ghost(9, 9, mazeView, level);
+                ghosts[3] = new Ghost(9, 6, mazeView, level);
                 break;
             case 4,5:
                 ghosts[0] = new Ghost(17, 17, mazeView, 3);
@@ -69,30 +57,10 @@ public class GamePane extends Pane {
                 ghosts[1].setMode(2);
                 ghosts[2] = new Ghost(1, 17, mazeView, 3);
                 ghosts[2].setMode(3);
-                ghosts[3] = new Ghost(9, 9, mazeView, level);
+                ghosts[3] = new Ghost(9, 6, mazeView, level);
                 break;
         }
         return ghosts;
-    }
-    // Checking pacman's Life (Timeline)
-    private void checkLife() {
-        death = false;
-        win = false;
-        sound.deathSound.stop();
-        sound.winSound.stop();
-        positionChecker = new Timeline(new KeyFrame(Duration.millis(40), e -> {
-            for (Ghost ghost : ghosts) {
-                if (ghost.getCurrentColumn() == pacman.getCurrentColumn() && ghost.getCurrentRow() == pacman.getCurrentRow()) {
-                    death = true;
-                    ghost.ghostMovement.stop();
-                }
-            }
-            if (death || pacman.getMazeView().getMaze().isFinishedMap()) {
-                startEndScene(death);
-            }
-        }));
-        positionChecker.setCycleCount(-1);
-        positionChecker.play();
     }
     // Responsible for moving the pacman (Timeline)
     public void movePacman() {
@@ -105,16 +73,43 @@ public class GamePane extends Pane {
                     pacman.setDirection(Direction.DOWN);    //to set last press down
                     break;
                 case UP:
-                    pacman.setDirection(Direction.UP);    //to set last press up
+                    pacman.setDirection(Direction.UP);     //to set last press up
                     break;
                 case LEFT:
                     pacman.setDirection(Direction.LEFT);    //to set last press left
                     break;
             }
         });
-
         pacman.getAutoMovement().setCycleCount(-1);
         pacman.getAutoMovement().play();
+    }
+
+    //-------------Helper Methods---------------//
+
+    private ImageView createBackground(int mazeNum) {
+        String[] mazes = new String[]{"GameBackground1.gif", "GameBackground2.gif", "GameBackground3.gif"};
+        ImageView background = new ImageView(mazes[mazeNum - 1]);
+        background.setFitWidth(BOARD_WIDTH);
+        background.setFitHeight(BOARD_HEIGHT);
+        return background;
+    }
+    private void checkLife() {
+        death = false;
+        sound.deathSound.stop();
+        sound.winSound.stop();
+        positionChecker = new Timeline(new KeyFrame(Duration.millis(40), e -> {
+            for (Ghost ghost : ghosts) {
+                if (ghost.getCurrentColumn() == pacman.getCurrentColumn() && ghost.getCurrentRow() == pacman.getCurrentRow()) {
+                    death = true;
+                    ghost.stopGhostMovement();
+                }
+            }
+            if (death || pacman.getMazeView().getMaze().isFinishedMap()) {
+                startEndScene(death);
+            }
+        }));
+        positionChecker.setCycleCount(-1);
+        positionChecker.play();
     }
     // Viewing Ending Screens [Win - Game Over]
     private void startEndScene(boolean isDeath) {
@@ -150,83 +145,6 @@ public class GamePane extends Pane {
         } else {
             handleGameWin(endingImgs, endGameTexts, txtsFadeTransition, txtsStrokeTransition, imgsTransition);
         }
-    }
-
-    // Set Winning Screen With All Effects Related.
-    private void handleGameWin(ImageView[] endingImgs, Text[] endGameTexts, FadeTransition[] txtsFadeTransition,
-                               StrokeTransition[] txtsStrokeTransition, ScaleTransition[] imgsTransition) {
-        pacman.getAutoMovement().stop();
-        pacman.getChildren().remove(pacman.getGif());
-        this.getChildren().remove(pacman);
-        // Win
-        endingImgs[1].setVisible(true);
-        imgsTransition[1].play();
-        imgsTransition[1].setOnFinished(event1 -> {
-            // Press Enter Text
-            endGameTexts[0].setVisible(true);
-            txtsFadeTransition[0].play();
-            txtsStrokeTransition[0].play();
-            // Press ESC Text
-            endGameTexts[1].setVisible(true);
-            txtsFadeTransition[1].play();
-            txtsStrokeTransition[1].play();
-            // You Won Text
-            endGameTexts[2].setVisible(true);
-            txtsFadeTransition[2].play();
-            txtsStrokeTransition[2].play();
-
-            gameScene.setOnKeyPressed(keyEvent -> {
-                switch (keyEvent.getCode()) {
-                    case ENTER -> {
-                        stage.setScene(mainScene);
-                        sound.winSound.stop();
-                    }
-                    case ESCAPE -> stage.close();
-                }
-
-            });
-        });
-        positionChecker.stop();
-        sound.winSound.play();
-        win = false;
-    }
-
-    // Setting Game Over Screen With All Effects Related.
-    private void handleGameOver( ImageView[] endingImgs, Text[] endGameTexts, FadeTransition[] txtsFadeTransition,
-                                 StrokeTransition[] txtsStrokeTransition, ScaleTransition[] imgsTransition) {
-        sound.deathSound.play();
-        death = false;
-
-        pacman.getAutoMovement().stop();
-        pacman.getChildren().remove(pacman.getGif());
-        this.getChildren().remove(pacman);
-
-        // Game Over
-        endingImgs[0].setVisible(true);
-        imgsTransition[0].play();
-        imgsTransition[0].setOnFinished(event1 -> {
-            // Press Enter Text
-            endGameTexts[0].setVisible(true);
-            txtsFadeTransition[0].play();
-            txtsStrokeTransition[0].play();
-            // Press ESC Text
-            endGameTexts[1].setVisible(true);
-            txtsFadeTransition[1].play();
-            txtsStrokeTransition[1].play();
-
-            gameScene.setOnKeyPressed(keyEvent -> {
-                switch (keyEvent.getCode()) {
-                    case ENTER -> {
-                        stage.setScene(mainScene);
-                        sound.deathSound.stop();
-                    }
-                    case ESCAPE -> {
-                        stage.close();
-                    }
-                }
-
-            });
-        });
     }
     // Setting Ending Texts [Press ESC - Press Enter - You Won] and Their Effects
     private Text[] initializeEndGameTexts(StrokeTransition[] txtsStrokeTransition, FadeTransition[] txtsFadeTransition) {
@@ -271,6 +189,79 @@ public class GamePane extends Pane {
         endGameTexts[2].setFill(Color.GOLD);
         txtsStrokeTransition[2].setFromValue(Color.LIGHTGOLDENRODYELLOW);
         return endGameTexts;
+    }
+    // Setting Winning Screen With All Effects Related.
+    private void handleGameWin(ImageView[] endingImgs, Text[] endGameTexts, FadeTransition[] txtsFadeTransition,
+                               StrokeTransition[] txtsStrokeTransition, ScaleTransition[] imgsTransition) {
+        pacman.getAutoMovement().stop();
+        pacman.getChildren().remove(pacman.getGif());
+        this.getChildren().remove(pacman);
+        // Win
+        endingImgs[1].setVisible(true);
+        imgsTransition[1].play();
+        imgsTransition[1].setOnFinished(event1 -> {
+            // Press Enter Text
+            endGameTexts[0].setVisible(true);
+            txtsFadeTransition[0].play();
+            txtsStrokeTransition[0].play();
+            // Press ESC Text
+            endGameTexts[1].setVisible(true);
+            txtsFadeTransition[1].play();
+            txtsStrokeTransition[1].play();
+            // You Won Text
+            endGameTexts[2].setVisible(true);
+            txtsFadeTransition[2].play();
+            txtsStrokeTransition[2].play();
+
+            gameScene.setOnKeyPressed(keyEvent -> {
+                switch (keyEvent.getCode()) {
+                    case ENTER -> {
+                        stage.setScene(mainScene);
+                        sound.winSound.stop();
+                    }
+                    case ESCAPE -> stage.close();
+                }
+
+            });
+        });
+        positionChecker.stop();
+        sound.winSound.play();
+    }
+    // Setting Game Over Screen With All Effects Related.
+    private void handleGameOver( ImageView[] endingImgs, Text[] endGameTexts, FadeTransition[] txtsFadeTransition,
+                                 StrokeTransition[] txtsStrokeTransition, ScaleTransition[] imgsTransition) {
+        sound.deathSound.play();
+        death = false;
+
+        pacman.getAutoMovement().stop();
+        pacman.getChildren().remove(pacman.getGif());
+        this.getChildren().remove(pacman);
+
+        // Game Over
+        endingImgs[0].setVisible(true);
+        imgsTransition[0].play();
+        imgsTransition[0].setOnFinished(event1 -> {
+            // Press Enter Text
+            endGameTexts[0].setVisible(true);
+            txtsFadeTransition[0].play();
+            txtsStrokeTransition[0].play();
+            // Press ESC Text
+            endGameTexts[1].setVisible(true);
+            txtsFadeTransition[1].play();
+            txtsStrokeTransition[1].play();
+
+            gameScene.setOnKeyPressed(keyEvent -> {
+                switch (keyEvent.getCode()) {
+                    case ENTER -> {
+                        stage.setScene(mainScene);
+                        sound.deathSound.stop();
+                    }
+                    case ESCAPE -> {
+                        stage.close();
+                    }
+                }
+            });
+        });
     }
 
 }
