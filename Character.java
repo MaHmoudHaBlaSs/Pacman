eb683e2020290b3a9daa90496071041379772471
Mahmoud Gamal <68353909+MaHmoudHaBlaSs@users.noreply.github.com>
2024-05-17 21:13:43 +0300

    Update Character.java

--- Character.java
+++ Character.java
@@ -8,28 +8,28 @@ import javafx.util.Duration;
 
 public class Character extends Pane {
     protected static final int CELL_SIZE = 40;
+    protected Direction direction;
     private ImageView gif ;
     protected int currentRow = 0;
     protected int currentColumn = 0;
     protected int nextRow = 0;
     protected int nextColumn = 0;
+    private int counter = 0;
     private boolean reflected = false;
     private Timeline moverTl;
+
     private double dx;
     private double dy;
-    private Timeline positionTl;
-    int counter = 0;
-    protected Direction direction;
+
 
     public Character(){
         setMoverTl();
-        setPositionTl();
     }
 
     private void setMoverTl(){
         // moverTl  Changes Character Gif Position Every 10ms by 2 Pixels For The Cycle
         // So It Should be Played For 20 Times to Reach 40 Pixels(CELL_WIDTH) [Changes currentRow - currentColumn]
-        moverTl =  new Timeline(new KeyFrame(Duration.millis(10), e-> move()));
+        moverTl =  new Timeline(new KeyFrame(Duration.millis(10.1), e-> move()));
         moverTl.setCycleCount(CELL_SIZE/2);
         moverTl.setOnFinished(e->{
             counter =0;
@@ -45,15 +45,7 @@ public class Character extends Pane {
             currentColumn = nextColumn;
         }
     }
-    private void setPositionTl(){
-        // positionTl Changes Character Gif Coordinates Through a Certain Amount of Time (190ms)
-        // [Doesn't Change currentRow - currentColumn]
-        positionTl = new Timeline(new KeyFrame(Duration.millis(250), e->{
-            gif.setX(currentColumn*CELL_SIZE +5);
-            gif.setY(currentRow*CELL_SIZE);}
-        ));
-        positionTl.setCycleCount(1);
-    }
+
     protected void setPosition(int row,int col){
         // Difference in Coordinates [Index]
         dx = col-currentColumn ;
@@ -65,7 +57,6 @@ public class Character extends Pane {
 
         moverTl.play();
     }
-    protected void setPosition(){positionTl.play();}
 
     /*-------------Gif Orientation Methods---------------*/
     public void reflectVerticallyToLeft() {
@@ -97,15 +88,11 @@ public class Character extends Pane {
         // Flip the Pacman image horizontally to the top
         gif.setRotate(270);
     }
-    /*--------------------------------------------------*/
-
 
     /*-----------------Setters / Getters-----------------*/
     public int getCurrentColumn() {return currentColumn;}
     public int getCurrentRow() {return currentRow;}
     public ImageView getGif(){return gif;}
-    public Direction getDirection() {return direction;}
     public void setGif(ImageView gif) {this.gif = gif;}
     public void setDirection(Direction direction) {this.direction = direction;}
-    /*-------------------------------------------------*/
 }
